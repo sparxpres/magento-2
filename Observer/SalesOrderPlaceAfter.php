aa3f780199509f9466da5f5ede0b2db86b8bbdbc
sparxpres <support@sparxpres.dk>
2024-05-29 10:55:43 +0200

    Also setCanSendNewEmailFlag for XpresPay

--- Observer/SalesOrderPlaceAfter.php
+++ Observer/SalesOrderPlaceAfter.php
@@ -5,6 +5,7 @@ use Sparxpres\Websale\Model\SparxpresPaymentMethod;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
+use Sparxpres\Websale\Model\XpresPayPaymentMethod;
 
 class SalesOrderPlaceAfter implements ObserverInterface
 {
@@ -30,7 +31,8 @@ class SalesOrderPlaceAfter implements ObserverInterface
         try {
             $order = $observer->getEvent()->getOrder();
             $paymentMethod = $order->getPayment()->getMethodInstance()->getCode();
-            if ($paymentMethod == SparxpresPaymentMethod::PAYMENT_METHOD_CODE) {
+            if ($paymentMethod == SparxpresPaymentMethod::PAYMENT_METHOD_CODE ||
+                $paymentMethod == XpresPayPaymentMethod::PAYMENT_METHOD_CODE) {
                 $order->setCanSendNewEmailFlag(false);
                 $this->orderRepository->save($order);
             }
